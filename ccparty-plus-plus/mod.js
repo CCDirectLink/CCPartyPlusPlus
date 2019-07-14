6a89eeddb02ecaed3ccabe50f0f8d01468540388
ac2pic <ac2pic@gmail.com>
2019-07-14 15:12:34 -0500

    Updated code to reflect new knowledge

--- ccparty-plus-plus/mod.js
+++ ccparty-plus-plus/mod.js
@@ -1,23 +1,23 @@
-document.body.addEventListener('modsLoaded', function() {
-	function generateRange(min, max, count) {
-		let interval = (max - min)/(count - 1);
-		return Array(count).fill(max).map((val, index) => val - index * interval);
-	}
-	const MAX_MEMBERS = 5;
-	
-	let damageOutputMultArr = generateRange(0.6, 1, MAX_MEMBERS);
-	let getDmgFactor = sc.party.getDmgFactor.bind(sc.party);
-	
-	sc.party.getDmgFactor = function() {
+import Config from "./config.json";
+
+
+function generateRange(min, max, count) {
+	let interval = (max - min)/(count - 1);
+	return Array(count).fill(max).map((val, index) => val - index * interval);
+}
+
+let damageOutputMultArr = generateRange(0.6, 1, Config.MAX_MEMBERS);
+sc.PARTY_MAX_MEMBERS = Config.MAX_MEMBERS;
+
+sc.PartyModel.inject({
+	getDmgFactor: function() {
 		
-		const trueDmgFactor = getDmgFactor();
-		const newDmgFactor = damageOutputMultArr[sc.party.getPartySize()];
+		const trueDmgFactor = this.parent();
+		const newDmgFactor = damageOutputMultArr[this.getPartySize()];
 		
 		if(trueDmgFactor > newDmgFactor) {
 			return trueDmgFactor;
 		}
 		return newDmgFactor;
-	};
-	
-	sc.PARTY_MAX_MEMBERS = damageOutputMultArr.length;
+	}
 });
